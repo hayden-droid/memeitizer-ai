1653068f50475d879f1608496de00d8bf0a7742f
Descendo The Monkei <61548447+hayden-droid@users.noreply.github.com>
2024-04-17 14:23:52 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDP3f1KSdYiqD5fCE1vgkxWanFiCTJ6aAg",
+  authDomain: "memeitizer-22285.firebaseapp.com",
+  projectId: "memeitizer-22285",
+  storageBucket: "memeitizer-22285.appspot.com",
+  messagingSenderId: "63093018518",
+  appId: "1:63093018518:web:554c06c7234a7a3b68d7b7",
+  measurementId: "G-C5C1RSQB03"
+};
